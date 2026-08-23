cf441afbe1563687d2ccf58b42878d74857b07fb
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-23 20:18:23 -0300

    feat(autor-repository): adiciona extends para jpa specification

--- src/main/java/dev/joaorooliveira/biblioteca_pessoal/repository/AutorRepository.java
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/repository/AutorRepository.java
@@ -2,6 +2,7 @@ package dev.joaorooliveira.biblioteca_pessoal.repository;
 
 import dev.joaorooliveira.biblioteca_pessoal.domain.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AutorRepository extends JpaRepository<Autor, Long> {
+public interface AutorRepository extends JpaRepository<Autor, Long>, JpaSpecificationExecutor<Autor> {
 }
