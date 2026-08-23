d14ccb18678317abf9593a2ee6156611fcd13e53
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-23 20:06:05 -0300

    feat(autor-service): adiciona metodo para salvar autor

--- src/main/java/dev/joaorooliveira/biblioteca_pessoal/service/AutorService.java
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/service/AutorService.java
@@ -1,4 +1,27 @@
 package dev.joaorooliveira.biblioteca_pessoal.service;
 
+import dev.joaorooliveira.biblioteca_pessoal.domain.Autor;
+import dev.joaorooliveira.biblioteca_pessoal.dto.AutorRequestDTO;
+import dev.joaorooliveira.biblioteca_pessoal.dto.AutorResponseDTO;
+import dev.joaorooliveira.biblioteca_pessoal.repository.AutorRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    @Transactional
+    public AutorResponseDTO salvarAutor(AutorRequestDTO dto) {
+        Autor autor = autorRepository.save(dto.toEntity());
+        return AutorResponseDTO.fromEntity(autor);
+    }
+
+
+
 }
