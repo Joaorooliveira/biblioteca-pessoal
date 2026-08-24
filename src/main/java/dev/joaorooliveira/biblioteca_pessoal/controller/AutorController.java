fa5ce061e82366345abe9e0768c02f9f7bb1aa36
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-23 21:40:03 -0300

    feat(autor-controller): adiciona endpoint para salvar autor

--- src/main/java/dev/joaorooliveira/biblioteca_pessoal/controller/AutorController.java
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/controller/AutorController.java
@@ -6,6 +6,7 @@ import dev.joaorooliveira.biblioteca_pessoal.service.AutorService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class AutorController {
         this.autorService = autorService;
     }
 
+    @PostMapping
     public ResponseEntity<AutorResponseDTO> salvar(@RequestBody @Valid AutorRequestDTO autorRequestDTO) {
         var autorResponseDTO = autorService.salvarAutor(autorRequestDTO);
 
