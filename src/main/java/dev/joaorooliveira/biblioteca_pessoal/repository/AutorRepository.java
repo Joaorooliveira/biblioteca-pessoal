416badfd33634f748cf0fd387923adf78bf0e40f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-24 20:55:22 -0300

    feat(autor-repository): adiciona metodo para mostrar a quantidade de livros por autor

--- src/main/java/dev/joaorooliveira/biblioteca_pessoal/repository/AutorRepository.java
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/repository/AutorRepository.java
@@ -1,8 +1,24 @@
 package dev.joaorooliveira.biblioteca_pessoal.repository;
 
 import dev.joaorooliveira.biblioteca_pessoal.domain.Autor;
+import dev.joaorooliveira.biblioteca_pessoal.projection.QuantidadeLivrosPorAutorProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long>, JpaSpecificationExecutor<Autor> {
+
+
+    @Query(value = """
+            SELECT a.nome,count(l) AS quantidade
+            FROM livros l
+            JOIN autores a
+            ON l.autor_id = a.id
+            GROUP BY nome
+            """,
+            nativeQuery = true
+    )
+    List<QuantidadeLivrosPorAutorProjection> quantidadeLivrosPorAutor();
 }
