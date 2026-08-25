cce3e2c27f62df37b81d5233962a4ef678c11d96
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-24 22:43:16 -0300

    feat: adiciona entidade nao encontrada exception

---
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package dev.joaorooliveira.biblioteca_pessoal.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/dev/joaorooliveira/biblioteca_pessoal/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package dev.joaorooliveira.biblioteca_pessoal.exception;
+
+public class GlobalExceptionHandler {
+}
